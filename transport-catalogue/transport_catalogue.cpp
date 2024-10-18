ad3163ec91aa832caf508ca4c750fd203e064f7d
VadimHub31 <164235555+VadimHub31@users.noreply.github.com>
2024-10-19 02:02:54 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -11,8 +11,8 @@ void TransportCatalogue::AddBus(const Bus& bus_to_add) {
     busname_to_bus_[buses_.back().name] = &buses_.back();
 
     for (Stop* stop : bus_to_add.stops) {
-        if (!count(stop_to_buses_.at(stop).begin(), stop_to_buses_.at(stop).end(), const_cast<Bus*>(FindBus(bus_to_add.name)))) {
-            stop_to_buses_[stop].push_back(const_cast<Bus*>(FindBus(move(bus_to_add).name)));
+        if (!count(stop_to_buses_.at(stop).begin(), stop_to_buses_.at(stop).end(), FindBus(bus_to_add.name))) {
+            stop_to_buses_[stop].push_back(busname_to_bus_[buses_.back().name]);
         }
     }
 }
@@ -21,11 +21,11 @@ void TransportCatalogue::AddStop(const Stop& stop_to_add) {
     stops_.push_back(stop_to_add);
     stopname_to_stop_[stops_.back().name] = &stops_.back();
 
-    Stop* stop = const_cast<Stop*>(FindStop(stop_to_add.name));
+    Stop* stop = stopname_to_stop_[stops_.back().name];
     stop_to_buses_[stop] = {};
 }
 
-const Bus* TransportCatalogue::FindBus(string_view busname) const {
+Bus* TransportCatalogue::FindBus(string_view busname) const {
     auto it = busname_to_bus_.find(busname);
     if (it != busname_to_bus_.end()) {
         return it->second;
@@ -33,7 +33,7 @@ const Bus* TransportCatalogue::FindBus(string_view busname) const {
     return nullptr;
 }
 
-const Stop* TransportCatalogue::FindStop(string_view stopname) const {
+Stop* TransportCatalogue::FindStop(string_view stopname) const {
     auto it = stopname_to_stop_.find(stopname);
     if (it != stopname_to_stop_.end()) {
         return it->second;
@@ -41,12 +41,12 @@ const Stop* TransportCatalogue::FindStop(string_view stopname) const {
     return nullptr;
 }
 
-const BusInfo TransportCatalogue::GetBusInfo(const Bus& bus) const {
+BusInfo TransportCatalogue::GetBusInfo(const Bus& bus) const {
     return {GetStopsNum(bus), GetUniqueStopsNum(bus), GetRouteLength(bus)};
 }
 
 std::vector<Bus*> TransportCatalogue::GetStopInfo(const Stop& stop) const {
-    return stop_to_buses_.at(const_cast<Stop*>(FindStop(stop.name)));
+    return stop_to_buses_.at(FindStop(stop.name));
 }
 
 }
