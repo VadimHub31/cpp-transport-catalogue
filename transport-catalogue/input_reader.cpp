70197e85b67ebea8ce65ea0ff37adebd3889e54d
VadimHub31 <164235555+VadimHub31@users.noreply.github.com>
2024-11-11 18:49:22 +0300

    Update input_reader.cpp

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cassert>
 #include <iterator>
+#include <string>
 
 namespace transport_catalogue {
 
@@ -98,6 +99,50 @@ CommandDescription ParseCommandDescription(std::string_view line) {
             std::string(line.substr(colon_pos + 1))};
 }
 
+std::string_view DistancesOutOfDescription(std::string_view command_description) {
+    auto first_comma = command_description.find_first_of(',');
+    auto second_comma = command_description.find_first_of(',', first_comma + 1);
+    if (second_comma != std::string::npos) {
+        auto start_dist = command_description.find_first_not_of(", ", second_comma);
+        auto end_dist = command_description.find_last_not_of(", ");
+        auto distances = command_description.substr(start_dist, end_dist - start_dist + 1);
+        return distances;
+    }
+    return {};
+}
+
+std::vector<std::string_view> DistVec(std::string_view distances) {
+    std::vector<std::string_view> result;
+    auto start = distances.find_first_not_of(", ");
+    auto first_comma = distances.find_first_of(',');
+    while (first_comma != std::string::npos) {
+        result.push_back(distances.substr(start, first_comma - start));
+        start = distances.find_first_not_of(", ", first_comma);
+        first_comma = distances.find_first_of(',', first_comma + 1);
+    }
+    result.push_back(distances.substr(start, distances.find_last_not_of(' ') - start + 1));
+    return result;
+}
+
+std::unordered_map<std::string_view, int> ParseDistances(std::string_view command_description) {
+    std::unordered_map<std::string_view, int> result;
+    auto distance_out_of_description = DistancesOutOfDescription(command_description);
+    if (distance_out_of_description.size() == 0) {
+        return {};
+    }
+    auto vector_of_distances = DistVec(distance_out_of_description);
+    for (auto distance : vector_of_distances) {
+        auto m_pos = distance.find_first_of('m');
+        int distancee = std::stoi(std::string{distance.substr(0, m_pos)});
+        auto first_space = distance.find_first_of(' ');
+        auto second_space = distance.find_first_of(' ', first_space + 1);
+        auto stop = distance.substr(second_space + 1, distance.find_last_not_of(' ') - second_space);
+        result[stop] = distancee;
+    }
+
+    return result;
+}
+
 } //namespace detail
 
 using namespace detail;
@@ -112,15 +157,29 @@ void InputReader::ParseLine(std::string_view line) {
 void InputReader::ApplyCommands([[maybe_unused]] TransportCatalogue& catalogue) const {
     // Реализуйте метод самостоятельно
     std::vector<CommandDescription> bus_commands;
+    std::vector<CommandDescription> stop_commands;
+
     for (const auto& command : commands_) {
         if (command.command == "Stop") {
             catalogue.AddStop({command.id, ParseCoordinates(command.description)});
+            stop_commands.push_back(command);
         }
         else if (command.command == "Bus") {
             bus_commands.push_back(command);
         }
     }
-    
+
+    for (const auto& stop_command : stop_commands) {
+        auto distances = ParseDistances(stop_command.description);
+        if (distances.size() != 0) {
+            for (const auto& [stop, distance] : distances) {
+                auto first_stop = catalogue.FindStop(stop_command.id);
+                auto second_stop = catalogue.FindStop(stop);
+                catalogue.SetStopsDistance({first_stop, second_stop}, distance);
+            }
+        }
+    }
+
     for (const auto& bus_command : bus_commands) { 
         std::vector<const Stop*> stops;  
 
